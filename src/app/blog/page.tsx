e8ab97b5b496b25973e30b68a857587216691742
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2025-08-24 15:15:21 +0800

    removed unused imports to fix build errors

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { getBlogPosts } from '@/lib/notion';
 import Link from 'next/link';
-import { FaCode, FaFutbol, FaChartLine, FaBook, FaLightbulb, FaHeart } from 'react-icons/fa';
+import { FaCode, FaFutbol, FaBook, FaLightbulb, FaHeart } from 'react-icons/fa';
 
 export default async function BlogPage() {
   const posts = await getBlogPosts();
