28415be391825baec5c649336502925666a4c1bf
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2026-04-02 23:23:40 +0800

    hotfix: correct cpf and accenture experience mapping

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -8,10 +8,9 @@ const experiences = [
     startDate: "December 2025",
     endDate: "Present",
     description: [
-      "Designed, developed, and optimized Java Spring Batch jobs for large-scale data processing, and gathered business requirements in collaboration with cross-functional teams for a government client",
-      "Configured batch workflows with AWS Step Functions, resolved production level defects and supported deployments across DEV, SIT, UAT, and PROD environments for different microservices in the backend system",
-      "Improved backend system performance by optimising SQL queries and database indexing, achieving 40% reduction in runtimes",
-      "Built Python-based AWS Lambda functions to automate API execution from S3-stored inputs, accelerating data processing workflows and eliminating manual intervention"
+      "Full stack development using Java Spring Boot and Angular for the Citizen Disbursement Web Portal",
+      "Designed and developed CI/CD pipeline improvements by incorporating functional regression testing using Playwright",
+      "Integrated Oobee into the CI/CD pipeline to enforce WCAG 2.1/2.2 A–AA accessibility standards for web applications, delivering a more inclusive browsing experience for persons with disabilities"
     ],
     tools: [techTools.github, techTools.java, techTools.aws, techTools.springboot, techTools.angular, techTools.selenium],
   },
@@ -22,9 +21,10 @@ const experiences = [
     startDate: "May 2025",
     endDate: "November 2025",
     description: [
-      "Full stack development using Java Spring Boot and Angular for the Citizen Disbursement Web Portal",
-      "Designed and developed CI/CD pipeline improvements by incorporating functional regression testing using Playwright",
-      "Integrated Oobee into the CI/CD pipeline to enforce WCAG 2.1/2.2 A–AA accessibility standards for web applications, delivering a more inclusive browsing experience for persons with disabilities"
+      "Designed, developed, and optimized Java Spring Batch jobs for large-scale data processing, and gathered business requirements in collaboration with cross-functional teams for a government client",
+      "Configured batch workflows with AWS Step Functions, resolved production level defects and supported deployments across DEV, SIT, UAT, and PROD environments for different microservices in the backend system",
+      "Improved backend system performance by optimising SQL queries and database indexing, achieving 40% reduction in runtimes",
+      "Built Python-based AWS Lambda functions to automate API execution from S3-stored inputs, accelerating data processing workflows and eliminating manual intervention"
     ],
     tools: [techTools.github, techTools.java, techTools.aws, techTools.springboot, techTools.angular],
   },
