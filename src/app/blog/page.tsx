88a55e0958a5296db10d6fd288cfa14057a8d813
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2026-04-18 12:27:43 +0800

    fix: make blog routes refresh from Notion

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import { FaCode, FaFutbol, FaHeart } from 'react-icons/fa';
 import { categorizePostCategory } from '@/lib/blogCategories';
 
+export const dynamic = 'force-dynamic';
+
 export default async function BlogPage() {
   let posts: Awaited<ReturnType<typeof getBlogPosts>> = [];
   let error: string | null = null;
